bd04cd38879ac6e10bc6a6966bb0147bf7b47da0
zhouli <545289437@qq.com>
2016-07-26 23:48:14 +0800

    zl

--- js/main.js
+++ js/main.js
@@ -6,10 +6,10 @@ $(function(){
 		'direction' : 'vertical'
 	});
 
-	var colors = [['#dfe8ed', '#30bae7'], ['#dfe8ed', '#d74680'], ['#dfe8ed', '#15c7a8'], ['#dfe8ed', '#eb7d4b'],
-				['#dfe8ed', '#FF00FF'], ['#dfe8ed', 'purple'], ['#dfe8ed', '#00F5FF'], ['#dfe8ed', 'mediumorchid']];                                                           
+	var colors = [['#D3B6C6', '#4B253A'],['#dfe8ed', '#30bae7'], ['#FCE6A4', '#EFB917'],['#dfe8ed', '#d74680'], ['#dfe8ed', '#15c7a8'], ['#dfe8ed', '#eb7d4b'],
+				['#dfe8ed', '#FF00FF'], ['#dfe8ed', 'purple'], ['#dfe8ed', '#00F5FF'], ['#BEE3F7', '#45AEEA'],['#F8F9B6', '#D2D558']];                                                           
 	
-	var percentage = [70,70,60,60,60,50,50,30];
+	var percentage = [60,70,60,60,40,30,30,10];
 	
 	var circles = [];
 	
@@ -24,6 +24,19 @@ $(function(){
 			colors:     colors[i - 1]
 		}));
 		
+	}
+	for(var i = 1; i <= 8; i++){
+		document.getElementById('add_button-'+i).onclick = function(e){
+			
+			var str= $(this).attr("id").substring(11);
+			
+			var ii = parseInt(str);
+			if(circles[ii-1]._value == 100){
+				circles[ii-1].update(0, 250);
+			}
+			circles[ii-1].update(circles[ii-1]._value + 10, 250);
+			circles[ii-1].updateColors(colors[circles[ii-1]._value/10]);
+		}
 	}
 
 });
