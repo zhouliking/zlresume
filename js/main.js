a412e49646adbeb8ee339950078ec1be65ebd3b5
ZhouLi <545289437@qq.com>
2016-07-26 16:27:56 +0800

    zl

--- js/main.js
+++ js/main.js
@@ -7,9 +7,9 @@ $(function(){
 	});
 
 	var colors = [['#dfe8ed', '#30bae7'], ['#dfe8ed', '#d74680'], ['#dfe8ed', '#15c7a8'], ['#dfe8ed', '#eb7d4b'],
-								['#dfe8ed', '#FF00FF'], ['#dfe8ed', 'purple'], ['#dfe8ed', '#00F5FF'], ['#dfe8ed', 'mediumorchid'],['#dfe8ed', 'fuchsia']];                                                           
+				['#dfe8ed', '#FF00FF'], ['#dfe8ed', 'purple'], ['#dfe8ed', '#00F5FF'], ['#dfe8ed', 'mediumorchid']];                                                           
 	
-	var percentage = [70,70,60,60,60,50,50,30,20];
+	var percentage = [70,70,60,60,60,50,50,30];
 	for (var i = 1; i <= 8; i++) {
 	  var child = document.getElementById('circles-' + i);                                     
 	  Circles.create({
