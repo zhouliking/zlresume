e6d17a3158f68e6a0e74ec3192a1282d97405e53
zhouli <545289437@qq.com>
2016-07-26 21:49:00 +0800

    zl

--- js/main.js
+++ js/main.js
@@ -9,18 +9,21 @@ $(function(){
 	var colors = [['#dfe8ed', '#30bae7'], ['#dfe8ed', '#d74680'], ['#dfe8ed', '#15c7a8'], ['#dfe8ed', '#eb7d4b'],
 				['#dfe8ed', '#FF00FF'], ['#dfe8ed', 'purple'], ['#dfe8ed', '#00F5FF'], ['#dfe8ed', 'mediumorchid']];                                                           
 	
-	var percentage = [70,70,60,60,60,50,50,30];
+	var percentage1 = [70,70,60,60,60,50,50,30];
+	
+	var circles = [];
 	for (var i = 1; i <= 8; i++) {
 	  var child = document.getElementById('circles-' + i);                                     
-	  Circles.create({
+	  circles.push(Circles.create({
 	    id:         child.id,
-	    percentage: percentage[i - 1],
+		
+	    percentage: percentage1[i - 1],
 	    radius:     60,
 	    width:      12,
-	    number:     percentage[i - 1],
+	    number:     percentage1[i - 1],
 	    text:       '%',
 	    colors:     colors[i - 1]
-	  });
+	  }));
 	}
 });
 
